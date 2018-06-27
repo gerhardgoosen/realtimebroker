845c668b4f6f656d20fa3aa4a16d280a3f8760e2
Gerhard Goosen (ext.) <Gerhard.Goosen@partner.bmw.co.za>
2018-06-27 21:07:25 +0200

    added some redis store and fetch

--- Config.js
+++ Config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     ENV_NAME: process.env.ENV_NAME,
-    REDIS_ENDPOINT: process.env.REDIS_ENDPOINT || 'http://rtbrokercache.redis.cache.windows.net',
-    REDIS_PORT: process.env.REDIS_PORT || 6379,
+    REDIS_ENDPOINT: process.env.REDIS_ENDPOINT || 'https://rtbrokercache.redis.cache.windows.net',
+    REDIS_PORT: process.env.REDIS_PORT || 6380,
     // Controls how often clients ping back and forth
     HEARTBEAT_TIMEOUT: 8000,
     HEARTBEAT_INTERVAL: 4000
